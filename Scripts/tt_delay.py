22dacd326b2d6072e9a305df69239e966e3f3a1a
Fabio Molignoni <fabio.molignoni@studenti.unitn.it>
2019-10-18 19:20:28 +0200

    Add train schedule

--- Scripts/tt_delay.py
+++ Scripts/tt_delay.py
@@ -4,16 +4,19 @@
 
 import requests
 import json
+import sys
 
 url = "https://app-tpl.tndigit.it/gtlservice/trips?&stopId={stopId}&type=U&limit=10000&refDateTime=2019-{month}-{day}T00:10:00.151Z"
 
 
 with open("./tt_delay.txt", "w") as myfile:
+    index = 0
     with requests.Session() as s:
         r = json.loads(s.get("https://app-tpl.tndigit.it/gtlservice/stops").text)
         for i in r:
-            # print(r.index(i)/len(r))
-            string = s.get(url.format(stopId=i["stopId"], day=17, month=10)).text
+            string = s.get(
+                url.format(stopId=i["stopId"], day=sys.argv[1], month=sys.argv[2])
+            ).text
             if string != "":
                 r = json.loads(string)
                 for bus in r:
